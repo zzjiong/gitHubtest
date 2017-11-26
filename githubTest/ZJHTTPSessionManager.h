0c35f8a136dd48e836f98513a0c655cc7a79f2ca
zhoujiong <zhoujiong@zhoujiongdeMacBook-Pro.local>
2017-11-26 17:09:22 +0800

    pod

---
+++ githubTest/ZJHTTPSessionManager.h
@@ -0,0 +1,15 @@
+//
+//  ZJHTTPSessionManager.h
+//  githubTest
+//
+//  Created by zhoujiong on 2017/11/26.
+//  Copyright © 2017年 Finemap SCI-Tech Co.,ltd. All rights reserved.
+//
+
+#import <AFNetworking/AFNetworking.h>
+
+@interface ZJHTTPSessionManager : AFHTTPSessionManager
+
+- (void)test;
+
+@end
---
+++ githubTest/ZJHTTPSessionManager.m
@@ -0,0 +1,18 @@
+//
+//  ZJHTTPSessionManager.m
+//  githubTest
+//
+//  Created by zhoujiong on 2017/11/26.
+//  Copyright © 2017年 Finemap SCI-Tech Co.,ltd. All rights reserved.
+//
+
+#import "ZJHTTPSessionManager.h"
+
+@implementation ZJHTTPSessionManager
+
+- (void)test{
+    
+    NSLog(@"testSucess");
+}
+
+@end
